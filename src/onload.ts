64b5a85b200bb85399702650b524112b3674f8bf
rhipsalis <ryugi0304@gmail.com>
2022-05-08 06:33:40 +0900

    _-_-_-_-_-_-_-_-_-_-_-_

--- src/onload.ts
+++ src/onload.ts
@@ -3,10 +3,10 @@ import * as fun from './http-com';
 export default async (userId: string | null, body: HTMLElement): Promise <void> => {
   if(!userId || !userId.match(/[0-9a-f]{32}/)) {
     body.innerHTML = "";
-    const h1 = document.createElement("h1"), p0 = document.createElement("p");
+    const h1 = document.createElement("h1"), p = document.createElement("p");
     h1.innerHTML = "ERROR";
-    p0.innerHTML = "このサイトはLINE Botから送信されるURL以外からのアクセスを許可していません。";
-    body.appendChild(h1).appendChild(p0);
+    p.innerHTML = "このサイトはLINE Botから送信されるURL以外からのアクセスを許可していません。";
+    body.appendChild(h1).appendChild(p);
   }else if (localStorage.getItem("lock") === "true") {
     const lock_status = await fun.check();
     if (!lock_status) {
